984eff62990af611b48a9fa59df2d6e8ff073390
Suyog.B <suyogburadkar@gmail.com>
2024-04-14 13:04:12 +0530

    Test: Camera: Add record & save functionality with GPIO buttons
    Issue: Team_XFactor_GoogleGenAI#3
    
    This commit is intends to add record and save functions with camera
    which works with GPIO button inputs.

---
+++ Hardware/test/GPIO_Capture_Record_Camera.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+ *
+ *  Unit code for Camera Recording & Save images
+ *  with button clicks.
+ *  Copyright: suyogburadkar@gmail.com
+ *  Author: B Suyog
+ *  Date: 14th April 2024
+ *
+"""
+
+from picamera2 import Picamera2, Preview
+from picamera2.encoders import H264Encoder
+import time
+from datetime import datetime
+from gpiozero import Button
+from signal import pause
+
+picam2 = Picamera2()
+button = Button(17)
+button2 = Button(22)
+
+def capture():
+    print("Capture button pressed !! Capturing image !")
+    camera_config = picam2.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (1280, 720)}, display="lores")
+    picam2.configure(camera_config)
+    timestamp = datetime.now().isoformat()
+    picam2.start()
+    time.sleep(2) 
+    picam2.capture_file('/home/pi/data/%s.jpg' % timestamp)
+    print('Image saved successfully !!!!')
+    picam2.stop_preview()
+    picam2.stop()
+
+def record():
+    print("Record button pressed !! Starting video record !")
+    video_config = picam2.create_video_configuration()
+    picam2.configure(video_config)
+    timestp = datetime.now().isoformat()
+    encoder = H264Encoder(10000000)
+    picam2.start_recording(encoder, '/home/pi/data/%s.h264' % timestp)
+    time.sleep(10)
+    print("Video saved successfully !!!")
+    picam2.stop_recording()
+
+button.when_pressed = capture
+button2.when_pressed = record
+pause()
