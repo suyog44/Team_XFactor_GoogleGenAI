3fa244f317d05148feea742810554015e234f110
Suyog.B <suyogburadkar@gmail.com>
2024-04-26 00:56:51 +0530

    Test: Session: Add session id for session creation
    Issue: #3
    
    This commit is intends to add session id which creates and differentiate
    sessions for the user.

--- Software/test_app.py
+++ Software/test_app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-FILE: Goggle Working Code V1.0
-DATE: 18th April 2024
+FILE: Goggle Working Code V1.3
+DATE: 26th April 2024
 
 """
 import time
@@ -23,30 +23,61 @@ from vertexai.alloydb_access import AlloydbAccess
 
 from annoy import AnnoyIndex
 
+SESSION_FILE_PATH = "/content/session.log"
 
-# LLM Function
-# def encode_image_to_base64(image_path):
-#     with open(image_path, "rb") as image_file:
-#         encoded_string = base64.b64encode(image_file.read())
-#         encoded_string = encoded_string.decode('utf-8')
-#     return encoded_string
+def get_last_session_id():
+    try:
+        with open(SESSION_FILE_PATH, "r") as file:
+            last_session_id = file.read().strip()
+        return last_session_id
+    except FileNotFoundError:
+        return None
 
-# def call_llm_api(text_input, image_path=None):
-#     if image_path:
-#         encoded_image = encode_image_to_base64(image_path)
-#         payload = json.dumps({"text_input": text_input, "image_input": [encoded_image]})
-#     else:
-#         payload = json.dumps({"text_input": text_input})
+def save_session_id(session_id):
+    with open(SESSION_FILE_PATH, "w") as file:
+        file.write(session_id)
 
-#     url = 'https://asia-south1-indigo-bazaar-420408.cloudfunctions.net/openvertex_tesrun'
-#     headers = {'Content-Type': 'application/json'}
+def increment_session_id(session_id):
+    session_number = int(session_id.split("#")[1])
+    next_session_number = session_number + 1
+    return f"session#{next_session_number:05d}"
 
-#     response = requests.post(url, data=payload, headers=headers)
-#     print(response.text)
+# Read last session ID or generate new one
+last_session_id = get_last_session_id()
+if last_session_id:
+    session_id = last_session_id
+else:
+    session_id = "session#00001"  # Initial session ID
 
-# Speech-to-Text and Audio Recording Functions
+# Initialize MPR121
+i2c = busio.I2C(board.SCL, board.SDA)
+mpr121 = adafruit_mpr121.MPR121(i2c)
+
+# Camera and other initializations
+picam2 = Picamera2()
+camera_config = picam2.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (1280, 720)}, display="lores")
+picam2.configure(camera_config)
+picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
+
+# Vertex APIs initialization 
+vertexai_url = 'https://asia-south1-indigo-bazaar-420408.cloudfunctions.net/openvertex_tesrun'
+alloydb_url = 'https://asia-south1-indigo-bazaar-420408.cloudfunctions.net/alloydb_connect'
+vertexai_ = VertexaiAccess(vertexai_url)
+db_access = AlloydbAccess(alloydb_url)
+
+TABLE_NAME = "late_night_tests"
+
+image_captured = False
+
+index = AnnoyIndex(768, 'angular')  # Using angular distance
+
+def capture_image(image_path):
+    picam2.start()
+    picam2.capture_file(image_path)
+    print(f"Image captured and saved at: {image_path}")
+    image_captured = True
+
 def transcribe_audio(audio_file_path):
-    #client = speech.SpeechClient()
     client = speech.SpeechClient.from_service_account_file('test/key.json')
 
     with open(audio_file_path, "rb") as audio_file:
@@ -98,57 +129,8 @@ def record_audio(audio_file_path, record_seconds=10):
     wf.writeframes(b''.join(frames))
     wf.close()
 
-# Camera and MPR121 Initialization
-i2c = busio.I2C(board.SCL, board.SDA)
-mpr121 = adafruit_mpr121.MPR121(i2c)
-picam2 = Picamera2()
-camera_config = picam2.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (1280, 720)}, display="lores")
-picam2.configure(camera_config)
-picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
-
-# Vertex apis intialization 
-vertexai_url = 'https://asia-south1-indigo-bazaar-420408.cloudfunctions.net/openvertex_tesrun'
-alloydb_url = 'https://asia-south1-indigo-bazaar-420408.cloudfunctions.net/alloydb_connect'
-vertexai_ = VertexaiAccess(vertexai_url)
-db_access = AlloydbAccess(alloydb_url)
-
-TABLE_Name = "late_night_tests"
-# print(db_access.create_table(TABLE_Name , '(session_id VARCHAR , image_data TEXT ,image_description TEXT)'))
-
-image_captured = False
-
-session_id =  "122312121" #"seret"
-
-index = AnnoyIndex(768, 'angular')  # Using angular distance
-
-
-# Function to capture image with unique ID
-def capture_image(image_path):
-    picam2.start()
-    picam2.capture_file(image_path)
-    print(f"Image captured and saved at: {image_path}")
-    image_captured = True
-
-
-# Function to record video for 10 seconds
-def record_video():
-    video_path = f"/content/video_{int(time.time())}.h264"
-    picam2.start_recording(video_path)
-    time.sleep(10)
-    picam2.stop_recording()
-    print(f"Video recorded and saved at: {video_path}")
-
-def prompt_creator(data , query):
-
-    prompt = f"Based on the context delimited in backticks, answer the query. ```{data}``` {query}"
-    
-    return prompt
-    
-
-
 # Main loop
 while True:
-    # Check for touched electrodes
     if mpr121[0].value:
         image_id = int(time.time())
         image_path = f"/content/Image_{image_id}.jpg"
@@ -157,33 +139,24 @@ while True:
         response = vertexai_.send_query(["What is this image about?"], [image_path])
         print(response)
         embeds = vertexai_.generate_embeddings([response], task='RETRIEVAL_DOCUMENT')
-        print(db_access.update_table(TABLE_Name, session_id, encoded_image, response , embeds[0]))
-        # print(db_access.reindex_table(TABLE_Name))
-        #call_llm_api(["Analyze this image"], image_path)
+        print(db_access.update_table(TABLE_NAME, session_id, encoded_image, response , embeds[0]))
 
     elif mpr121[1].value:
-        #record_video()
         print("Video Recording is not available")
+
     elif mpr121[2].value:
-        # Record audio temporarily
         with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as audio_file:
             audio_file_path = audio_file.name
             record_audio(audio_file_path)
 
-        # Transcribe the audio
         text_input = transcribe_audio(audio_file_path)
 
-
         if text_input:
-
             stored_data = db_access.retrieve_session_data(session_id)
             embeds = stored_data["embedding"]
 
-            print(len(embeds))
-
             for i in range(len(embeds)):
                 embed = json.loads(embeds[i])
-                print(len(embed))
                 index.add_item(i, embed)
             index.build(10)  # 10 trees 
 
@@ -192,29 +165,16 @@ while True:
 
             data = "\n".join([stored_data["image_description"][index] for index in data_index[0] ])
 
-            # data = db_access.retrieve_from_table(TABLE_Name, text_input, 2 , 'image_description')
-            print("Data : " , data)
+            prompt = f"Based on the context delimited in backticks, answer the query. ```{data}``` {text_input}"
+            response = vertexai_.send_query([prompt], [])
 
-            prompt = prompt_creator(data , text_input)
-            response = vertexai_.send_query([prompt], [])  # Call LLM with transcribed text
-            print(response)
-
-            # Now, let's clear the Annoy index
             index = AnnoyIndex(768, 'angular')  # Using angular distance
 
-
- 
-
-            # del index
-
-
-    # if image_captured:
-    #     print("")
-    #     encoded_image = db_access.encode_image_to_base64(image_path)
-    #     response = vertexai_.send_query(["What is this image about?"], [image_path])
-    #     print(response)
-    #     print(db_access.update_table(TABLE_Name, "122312121", encoded_image, response))
-    #     print(db_access.reindex_table(TABLE_Name))
-    #     image_captured = False
+    elif mpr121[3].value:
+        # Generate new session ID
+        session_id = increment_session_id(session_id)
+        save_session_id(session_id)
+        print("New session ID:", session_id)
 
     time.sleep(0.1) # Adjust delay as needed
+
